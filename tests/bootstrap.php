eb02c9fbd013756acc16dca866e5ae8303ce4a4b
kamildevp <kamil.k52645@gmail.com>
2025-08-14 08:53:24 +0200

    Test DB setup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,3 +11,19 @@ if (method_exists(Dotenv::class, 'bootEnv')) {
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+
+passthru(sprintf(
+    'php "%s/../bin/console" doctrine:database:create --env=test --if-not-exists',
+    __DIR__
+));
+
+passthru(sprintf(
+    'php "%s/../bin/console" --env=test doctrine:schema:update --complete --force',
+    __DIR__
+));
+
+passthru(sprintf(
+    'APP_ENV=%s php "%s/../bin/console" cache:clear --no-warmup',
+    $_ENV['APP_ENV'],
+    __DIR__
+));

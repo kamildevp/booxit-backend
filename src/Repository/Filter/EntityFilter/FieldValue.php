ac3211643e50d408b7a5dd1420e9d02f4edf9765
kamildevp <kamil.k52645@gmail.com>
2025-08-16 23:36:43 +0200

    Added additional types support in FieldValue filter

--- src/Repository/Filter/EntityFilter/FieldValue.php
+++ src/Repository/Filter/EntityFilter/FieldValue.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository\Filter\EntityFilter;
 
+use DateTimeInterface;
 use Doctrine\ORM\QueryBuilder;
 
 class FieldValue extends AbstractFieldFilter
@@ -9,6 +10,11 @@ class FieldValue extends AbstractFieldFilter
     public function __construct(public string $operator)
     {
         
+    }
+
+    public function supports(mixed $value): bool
+    {
+        return is_string($value) || is_numeric($value) || $value instanceof DateTimeInterface;
     }
 
     public function apply(QueryBuilder $qb, string $columnName, mixed $value, string $filterId): void

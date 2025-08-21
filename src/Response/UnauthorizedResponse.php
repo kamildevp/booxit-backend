b022ffb698387b9a9575f6fd6faa308df94f559e
kamildevp <kamil.k52645@gmail.com>
2025-08-21 20:25:13 +0200

    Moved Responses default messages to constants

--- src/Response/UnauthorizedResponse.php
+++ src/Response/UnauthorizedResponse.php
@@ -5,8 +5,9 @@ namespace App\Response;
 class UnauthorizedResponse extends ClientErrorResponse
 {
     public const RESPONSE_STATUS = 401;
+    public const RESPONSE_MESSAGE = 'Unauthorized';
 
-    public function __construct(string $message = 'Unauthorized', array $headers = [])
+    public function __construct(string $message = self::RESPONSE_MESSAGE, array $headers = [])
     {
         return parent::__construct(self::RESPONSE_STATUS, $message, null, $headers);
     }
--- src/Response/ValidationFailedResponse.php
+++ src/Response/ValidationFailedResponse.php
@@ -7,8 +7,9 @@ use App\Enum\ResponseStatus;
 class ValidationFailedResponse extends ApiResponse
 {
     public const RESPONSE_STATUS = 422;
+    public const RESPONSE_MESSAGE = 'Validation Failed';
 
-    public function __construct(string $message = 'Validation Failed', array $headers = [])
+    public function __construct(string $message = self::RESPONSE_MESSAGE, array $headers = [])
     {
         parent::__construct([
             'status' => ResponseStatus::FAIL, 

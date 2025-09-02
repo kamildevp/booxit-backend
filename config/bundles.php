7381ac78633a483aa71bc979e5102900af23ed30
kamildevp <kamil.k52645@gmail.com>
2025-09-02 18:23:08 +0200

    Added symfony web profiler

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];

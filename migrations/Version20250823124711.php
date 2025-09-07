26fb1f38ef7bab226ab964fc7942f14fac8ac8a0
kamildevp <kamil.k52645@gmail.com>
2025-09-07 15:11:47 +0200

    migration fix

--- migrations/Version20250823124711.php
+++ migrations/Version20250823124711.php
@@ -20,7 +20,8 @@ final class Version20250823124711 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, name VARCHAR(50) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, verified BOOLEAN NOT NULL, expiry_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL DEFAULT nextval(\'user_id_seq\'), email VARCHAR(180) NOT NULL, name VARCHAR(50) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, verified BOOLEAN NOT NULL, expiry_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
         $this->addSql('COMMENT ON COLUMN "user".created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN "user".updated_at IS \'(DC2Type:datetime_immutable)\'');
@@ -30,5 +31,6 @@ final class Version20250823124711 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
     }
 }
--- migrations/Version20250823130552.php
+++ migrations/Version20250823130552.php
@@ -20,7 +20,8 @@ final class Version20250823130552 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE email_confirmation (id INT NOT NULL, creator_id INT DEFAULT NULL, email VARCHAR(255) NOT NULL, params TEXT DEFAULT NULL, expiry_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, verification_handler VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE SEQUENCE email_confirmation_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE email_confirmation (id INT NOT NULL DEFAULT nextval(\'email_confirmation_id_seq\'), creator_id INT DEFAULT NULL, email VARCHAR(255) NOT NULL, params TEXT DEFAULT NULL, expiry_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, verification_handler VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_1D2EF46F61220EA6 ON email_confirmation (creator_id)');
         $this->addSql('COMMENT ON COLUMN email_confirmation.params IS \'(DC2Type:array)\'');
         $this->addSql('ALTER TABLE email_confirmation ADD CONSTRAINT FK_1D2EF46F61220EA6 FOREIGN KEY (creator_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -31,5 +32,6 @@ final class Version20250823130552 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE email_confirmation DROP CONSTRAINT FK_1D2EF46F61220EA6');
         $this->addSql('DROP TABLE email_confirmation');
+        $this->addSql('DROP SEQUENCE email_confirmation_id_seq CASCADE');
     }
 }

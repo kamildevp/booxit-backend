386ca99a0dd068bcbfe0f58f314002fe68e5b9f2
kamildevp <kamil.k52645@gmail.com>
2025-09-07 16:15:35 +0200

    Added message to NotFoundResponse constructor

--- src/Response/NotFoundResponse.php
+++ src/Response/NotFoundResponse.php
@@ -9,8 +9,8 @@ class NotFoundResponse extends ClientErrorResponse
     public const RESPONSE_STATUS = 404;
     public const RESPONSE_MESSAGE = 'Not Found';
 
-    public function __construct(array $headers = [])
+    public function __construct(string $message = self::RESPONSE_MESSAGE, array $headers = [])
     {
-        return parent::__construct(self::RESPONSE_STATUS, self::RESPONSE_MESSAGE, null, $headers);
+        return parent::__construct(self::RESPONSE_STATUS, $message, null, $headers);
     }
 } 

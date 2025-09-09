54c6a20b4d6d6d770a1a13046bd4a51f72b01420
kamildevp <kamil.k52645@gmail.com>
2025-09-09 12:24:22 +0200

    Used ValuesTrait in TimestampsColumns enum

--- src/Enum/TimestampsColumns.php
+++ src/Enum/TimestampsColumns.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace App\Enum;
 
+use App\Enum\Trait\ValuesTrait;
+
 enum TimestampsColumns: string
 {
+    use ValuesTrait;
+
     case CREATED_AT = 'created_at';
     case UPDATED_AT = 'updated_at';
-
-    public static function values(): array
-    {
-       return array_column(self::cases(), 'value');
-    }
 }

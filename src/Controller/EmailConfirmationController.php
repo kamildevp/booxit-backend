da9d706a9f3036fea0c78ef94c5fc6f92a5301f9
kamildevp <kamil.k52645@gmail.com>
2025-09-28 19:04:55 +0200

    Added validate EmailConfirmation endpoint tests and docs

--- src/Controller/EmailConfirmationController.php
+++ src/Controller/EmailConfirmationController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Documentation\Response\ServerErrorResponseDoc;
+use App\Documentation\Response\SuccessResponseDoc;
+use App\Documentation\Response\ValidationErrorResponseDoc;
 use App\DTO\EmailConfirmation\ValidateEmailConfirmationDTO;
 use App\Response\SuccessResponse;
 use App\Response\ValidationFailedResponse;
@@ -9,14 +12,26 @@ use App\Service\Entity\EmailConfirmationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Attributes as OA;
 
+#[ServerErrorResponseDoc()]
+#[OA\Tag('EmailConfirmation')]
 class EmailConfirmationController extends AbstractController
 {
+    #[OA\Get(
+        summary: 'Check email confirmation validity',
+        description: 'Checks whether the provided email confirmation parameters are valid. 
+        This endpoint only verifies the validity of the email confirmation and does not perform the actual verification process, 
+        which should be handled by other dedicated verification endpoints. 
+        It is intended for use when the client needs to confirm the validity of an email confirmation before initiating the full verification process.'
+    )]
+    #[SuccessResponseDoc(dataExample: ['valid' => true])]
+    #[ValidationErrorResponseDoc]
     #[Route('email_confirmation/validate', name: 'email_confirmation_validate', methods: ['GET'])]
-    public function verify(EmailConfirmationService $emailConfirmationService, #[MapQueryString] ValidateEmailConfirmationDTO $dto)
+    public function validate(EmailConfirmationService $emailConfirmationService, #[MapQueryString] ValidateEmailConfirmationDTO $dto)
     {
         $valid = $emailConfirmationService->validateEmailConfirmation($dto);
 
-        return $valid ? new SuccessResponse(['valid' => $valid]) : new ValidationFailedResponse();
+        return $valid ? new SuccessResponse(['valid' => true]) : new ValidationFailedResponse();
     }
 }
---
+++ tests/Feature/EmailConfirmation/EmailConfirmationControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Feature\EmailConfirmation;
+
+use App\DataFixtures\Test\User\PasswordResetFixtures;
+use App\DataFixtures\Test\User\VerifyUserEmailFixtures;
+use App\DataFixtures\Test\User\VerifyUserFixtures;
+use App\Enum\EmailConfirmationType;
+use App\Tests\Feature\EmailConfirmation\DataProvider\EmailConfirmationValidateDataProvider;
+use App\Tests\Utils\Attribute\Fixtures;
+use App\Tests\Utils\BaseWebTestCase;
+use App\Tests\Utils\Trait\EmailConfirmationUtils;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
+
+class EmailConfirmationControllerTest extends BaseWebTestCase
+{
+    use EmailConfirmationUtils;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    #[Fixtures([VerifyUserFixtures::class, VerifyUserEmailFixtures::class, PasswordResetFixtures::class])]
+    #[DataProviderExternal(EmailConfirmationValidateDataProvider::class, 'validDataCases')]
+    public function testValidateSuccess(EmailConfirmationType $type): void
+    {
+        $params = $this->prepareEmailConfirmationVerifyParams($type);
+        $responseData = $this->getSuccessfulResponseData($this->client, 'GET', '/api/email_confirmation/validate', $params);
+
+        $this->assertEquals(['valid' => true], $responseData);
+    }
+
+    #[Fixtures([VerifyUserFixtures::class, VerifyUserEmailFixtures::class, PasswordResetFixtures::class])]
+    #[DataProviderExternal(EmailConfirmationValidateDataProvider::class, 'failureDataCases')]
+    public function testValidateFailure(array $verifyParams, EmailConfirmationType $type): void
+    {
+        $validParams = $this->prepareEmailConfirmationVerifyParams($type);
+        $params = array_merge($validParams, $verifyParams);
+        $responseData = $this->getFailureResponseData($this->client, 'GET', '/api/email_confirmation/validate', $params);
+
+        $this->assertEquals('Validation Failed', $responseData['message']);
+    }
+
+    #[DataProviderExternal(EmailConfirmationValidateDataProvider::class, 'validationDataCases')]
+    public function testValidateValidation(array $params, array $expectedErrors): void
+    {
+        $this->assertPathValidation($this->client, 'GET', '/api/email_confirmation/validate', $params, $expectedErrors);
+    }
+}

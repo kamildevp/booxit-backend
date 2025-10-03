6fa72c44265de54f1af21b5c5f9e3dad584d9abb
kamildevp <kamil.k52645@gmail.com>
2025-10-03 19:17:48 +0200

    Added schedule management access rule

---
+++ src/Service/Auth/AccessRule/ScheduleManagementPrivilegesRule.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Auth\AccessRule;
+
+use App\Entity\User;
+use App\Enum\Organization\OrganizationRole;
+use App\Exceptions\ForbiddenException;
+use App\Exceptions\UnauthorizedException;
+use App\Repository\OrganizationMemberRepository;
+use App\Repository\OrganizationRepository;
+use App\Repository\ScheduleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ScheduleManagementPrivilegesRule implements AccessRuleInterface
+{
+    public function __construct(
+        protected OrganizationRepository $organizationRepository,
+        protected OrganizationMemberRepository $organizationMemberRepository,
+        protected ScheduleRepository $scheduleRepository
+    )
+    {
+        
+    }
+
+    public function validateAccess(?UserInterface $user, Request $request): void
+    {
+        if(!$user instanceof User){
+            throw new UnauthorizedException;
+        }
+
+        $requestContent = json_decode($request->getContent(), true) ?? [];
+        $organizationId = $requestContent['organization_id'] ?? null;
+        $scheduleId = $request->attributes->get('schedule');
+        $organization = !is_null($scheduleId) ? $this->scheduleRepository->findOrFail($scheduleId)->getOrganization() : null;
+        $organization = !$organization && is_int($organizationId) ? $this->organizationRepository->find($organizationId) : $organization;
+
+        $actionMember = $organization ? $this->organizationMemberRepository->findOneBy([
+            'organization' => $organization,
+            'appUser' => $user,
+        ]) : null;
+        
+        if(!$actionMember || $actionMember->getRole() != OrganizationRole::ADMIN->value){
+            throw new ForbiddenException;
+        }
+    }
+}

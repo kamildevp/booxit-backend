dc9ee6eb0e5ae399072ab54d6107bb14fbfcc749
kamildevp <kamil.k52645@gmail.com>
2025-10-04 16:48:31 +0200

    Simplified EntityValueResolver

--- src/ValueResolver/EntityValueResolver.php
+++ src/ValueResolver/EntityValueResolver.php
@@ -4,41 +4,26 @@ declare(strict_types=1);
 
 namespace App\ValueResolver;
 
-use App\Repository\RepositoryUtilsInterface;
+use App\Exceptions\EntityNotFoundException;
 use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\Mapping\MappingException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class EntityValueResolver implements ValueResolverInterface
 {
-    public function __construct(private EntityManagerInterface $entityManager) {}
+    public function __construct(private ValueResolverInterface $inner) {}
 
     public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
         $entityClass = $argument->getType();
-        $id = $request->attributes->get($argument->getName());
 
-        if (!$id || !$entityClass || !class_exists($entityClass)) {
-            return [];
+        try{
+            return $this->inner->resolve($request, $argument);
         }
-
-        try {
-            $metadata = $this->entityManager->getClassMetadata($entityClass);
-        } catch (MappingException) {
-            return [];
+        catch(NotFoundHttpException){
+            throw new EntityNotFoundException($entityClass);
         }
-
-        
-        $repository = $this->entityManager->getRepository($entityClass);
-        if(!$repository instanceof RepositoryUtilsInterface){
-            return [];
-        }
-
-        $entity = $repository->findOrFail($id);
-
-        return [$entity];
     }
 }
 

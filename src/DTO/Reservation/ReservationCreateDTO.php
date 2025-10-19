ce7d94f5e101bea090514ddf5213586138edb820
kamildevp <kamil.k52645@gmail.com>
2025-10-20 01:54:29 +0200

    Added reservation service

---
+++ src/DTO/Reservation/ReservationCreateDTO.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO\Reservation;
+
+use App\DTO\AbstractDTO;
+use App\DTO\Attribute\EntityReference;
+use App\DTO\EmailConfirmation\Trait\VerificationHandlerFieldDTO;
+use App\Entity\Schedule;
+use App\Entity\Service;
+use App\Validator\Constraints as CustomAssert;
+use App\Validator\Constraints\Compound as Compound;
+use Misd\PhoneNumberBundle\Validator\Constraints\PhoneNumber;
+use OpenApi\Attributes as OA;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ReservationCreateDTO extends AbstractDTO 
+{
+    use VerificationHandlerFieldDTO;
+
+    public function __construct(
+        #[CustomAssert\EntityExists(Schedule::class)]
+        #[EntityReference(Schedule::class, 'schedule')]
+        public readonly int $scheduleId,
+        #[CustomAssert\EntityExists(Service::class, relatedTo: ['schedules' => '{body:schedule_id}'])]
+        #[EntityReference(Service::class, 'service')]
+        public readonly int $serviceId,
+        #[OA\Property(format: 'email')]
+        #[Assert\Email(message: 'Parameter is not a valid email')]
+        public readonly string $email,
+        #[PhoneNumber]
+        public readonly string $phoneNumber,
+        #[Compound\DateTimeStringRequirements]
+        public readonly string $startDateTime,
+        string $verificationHandler
+    )
+    {
+        $this->verificationHandler = $verificationHandler;
+    }
+}
---
+++ src/Service/Entity/ReservationService.php
@@ -0,0 +1,144 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Entity;
+
+use App\DTO\Reservation\ReservationCreateDTO;
+use App\Entity\EmailConfirmation;
+use App\Service\EntitySerializer\EntitySerializerInterface;
+use App\Entity\Reservation;
+use App\Enum\EmailConfirmation\EmailConfirmationType;
+use App\Enum\EmailType;
+use App\Enum\Reservation\ReservationStatus;
+use App\Enum\Reservation\ReservationType;
+use App\Exceptions\ConflictException;
+use App\Message\ReservationVerificationMessage;
+use App\Repository\ReservationRepository;
+use App\Service\EmailConfirmation\EmailConfirmationHandlerInterface;
+use DateTime;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class ReservationService
+{
+    const DEFAULT_RESERVATION_VERIFICATION_EXPIRY = '+30 minutes';
+
+    public function __construct(
+        private EntitySerializerInterface $entitySerializer,
+        private AvailabilityService $availabilityService,
+        private EmailConfirmationService $emailConfirmationService,
+        private ReservationRepository $reservationRepository,
+        private EmailConfirmationHandlerInterface $emailConfirmationHandler,
+        private MessageBusInterface $messageBus,
+    )
+    {
+
+    }
+
+    public function createReservation(ReservationCreateDTO $dto): Reservation
+    {
+        $reservation = $this->makeReservation($dto);
+        $this->validateReservationAvailability($reservation);
+        
+        $reservation->setExpiryDate(new DateTime(self::DEFAULT_RESERVATION_VERIFICATION_EXPIRY));
+        
+        $this->reservationRepository->save($reservation, true);
+        $this->sendReservationVerification($reservation, $dto->verificationHandler);
+
+        return $reservation;
+    }
+
+    private function makeReservation(ReservationCreateDTO $dto): Reservation
+    {
+        $reservation = $this->entitySerializer->parseToEntity($dto, Reservation::class);
+        $endDateTime = $reservation->getStartDateTime()->add($reservation->getService()->getDuration());
+        $reference = $this->generateReservationReference($reservation);
+        
+        $reservation->setEndDateTime($endDateTime);
+        $reservation->setOrganization($reservation->getSchedule()->getOrganization());
+        $reservation->setReference($reference);
+        $reservation->setEstimatedPrice($reservation->getService()->getEstimatedPrice());
+        $reservation->setStatus(ReservationStatus::PENDING->value);
+        $reservation->setType(ReservationType::REGULAR->value);
+        $reservation->setVerified(false);
+
+        return $reservation;
+    }
+
+    private function validateReservationAvailability(Reservation $reservation): void
+    {
+        $startDateTime = $reservation->getStartDateTime();
+        $date = $startDateTime->format('Y-m-d');
+        $time = $startDateTime->format('H:i');
+        $availability = $this->availabilityService->getScheduleAvailability(
+            $reservation->getSchedule(), 
+            $reservation->getService(), 
+            $startDateTime, 
+            $reservation->getEndDateTime()
+        );
+
+        if(!isset($availability[$date]) || !in_array($time, $availability[$date])){
+            throw new ConflictException("Reservation time slot is not available.");
+        }
+    }
+
+    private function sendReservationVerification(Reservation $reservation, string $verificationHandler): void
+    {
+        $verificationEmailConfirmation = $this->createReservationVerification($reservation, $verificationHandler);
+        $cancellationEmailConfirmation = $this->createReservationCancellation($reservation, $verificationHandler);
+
+        $this->messageBus->dispatch(new ReservationVerificationMessage(
+            $verificationEmailConfirmation->getId(),
+            $reservation->getId(),
+            EmailType::RESERVATION_VERIFICATION->value,
+            $reservation->getEmail(),
+            [
+                'verification_url' => $this->emailConfirmationHandler->generateSignedUrl($verificationEmailConfirmation),
+                'verification_expiration_date' => $verificationEmailConfirmation->getExpiryDate(),
+                'cancellation_url' => $this->emailConfirmationHandler->generateSignedUrl($cancellationEmailConfirmation),
+                'cancellation_expiration_date' => $cancellationEmailConfirmation->getExpiryDate(),
+                'organization_name' => $reservation->getOrganization()->getName(),
+                'service_name' => $reservation->getService()->getName(),
+                'start_date_time' => $reservation->getStartDateTime(),
+                'estimated_price' => $reservation->getEstimatedPrice().' PLN',
+                'duration' => $reservation->getService()->getDuration()->format('%h:%ih'),
+            ]
+        ));
+    }
+
+    private function createReservationVerification(Reservation $reservation, string $verificationHandler): EmailConfirmation
+    {
+        return $this->emailConfirmationService->createEmailConfirmation(
+            $reservation->getEmail(),
+            $verificationHandler,
+            EmailConfirmationType::RESERVATION_VERIFICATION->value,
+            null,
+            $reservation->getExpiryDate(),
+            ['reservation_id' => $reservation->getId()]
+        );
+    }
+
+    private function createReservationCancellation(Reservation $reservation, string $verificationHandler): EmailConfirmation
+    {
+        return $this->emailConfirmationService->createEmailConfirmation(
+            $reservation->getEmail(),
+            $verificationHandler,
+            EmailConfirmationType::RESERVATION_CANCELLATION->value,
+            null,
+            $reservation->getStartDateTime(),
+            ['reservation_id' => $reservation->getId()]
+        );
+    }
+
+    private function generateReservationReference(Reservation $reservation): string
+    {
+        $uuid = Uuid::uuid4()->toString();
+        $date = $reservation->getStartDateTime()->format('Y-m-d');
+        $scheduleId = $reservation->getSchedule()->getId();
+        $serviceId = $reservation->getService()->getId();
+
+        return "RSV-$date{$scheduleId}{$serviceId}/".substr($uuid,0,8);
+    }
+
+}

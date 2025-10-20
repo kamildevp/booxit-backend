cbd17b008bb2b798b88a7a2f46152e2c75285283
kamildevp <kamil.k52645@gmail.com>
2025-10-20 15:59:43 +0200

    Added reservation controller test

---
+++ tests/Feature/Reservation/DataProvider/ReservationCreateDataProvider.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Feature\Reservation\DataProvider;
+
+use App\Enum\Reservation\ReservationStatus;
+use App\Tests\Utils\DataProvider\BaseDataProvider;
+use App\Validator\Constraints\Compound\DateTimeStringRequirements;
+use DateTimeImmutable;
+
+class ReservationCreateDataProvider extends BaseDataProvider 
+{
+    
+    public static function validDataCases()
+    {
+        $startDateTime = (new DateTimeImmutable('wednesday next week'))->setTime(2,0);
+        $endDateTime = (new DateTimeImmutable('wednesday next week'))->setTime(2,30);
+
+        return [
+            [
+                [
+                    'email' => 'user@example.com',
+                    'phone_number' => '+48213721372',
+                    'start_date_time' => $startDateTime->format(DateTimeStringRequirements::FORMAT),
+                    'verification_handler' => self::VERIFICATION_HANDLER,
+                ],
+                [
+                    'email' => 'user@example.com',
+                    'phone_number' => '+48213721372',
+                    'start_date_time' => $startDateTime->format(DateTimeStringRequirements::FORMAT),
+                    'end_date_time' => $endDateTime->format(DateTimeStringRequirements::FORMAT),
+                    'estimated_price' => '20.50',
+                    'status' => ReservationStatus::PENDING->value,
+                ]
+            ]
+        ];
+    }
+
+    public static function validationDataCases()
+    {
+        return [
+            [
+                [
+                    'schedule_id' => 0,
+                    'service_id' => 0,
+                    'email' => 'a',
+                    'phone_number' => 'a',
+                    'start_date_time' => 'a',
+                    'verification_handler' => 'a',
+                ],
+                [
+                    'schedule_id' => [
+                        'Schedule does not exist',
+                    ],
+                    'service_id' => [
+                        'Service does not exist',
+                    ],
+                    'email' => [
+                        'Parameter is not a valid email',
+                    ],
+                    'phone_number' => [
+                        'This value is not a valid phone number.',
+                    ],
+                    'start_date_time' => [
+                        'Parameter must be datetime string in format '. DateTimeStringRequirements::FORMAT
+                    ],
+                    'verification_handler' => [
+                        'Invalid verification handler'
+                    ]
+                ]
+            ],
+        ];
+    }
+}

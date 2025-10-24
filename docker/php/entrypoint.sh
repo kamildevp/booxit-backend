15e664ee44160b4fb2669ba94287e2327081d3c5
kamildevp <kamil.k52645@gmail.com>
2025-10-24 20:02:58 +0200

    Improved environment setup

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -2,9 +2,10 @@
 
 if [ ! -d /var/www/vendor ]; then
     composer install
+    php bin/console doctrine:migrations:migrate
 fi
 
-chown -R www-data:1000 /var/www/storage
-chown -R www-data:1000 /var/www/var
+chown -R www-data:$UID /var/www/storage
+chown -R www-data:$UID /var/www/var
 
 exec docker-php-entrypoint "$@"

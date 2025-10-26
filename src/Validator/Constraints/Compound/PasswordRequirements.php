15ea9e754ba3a13975a91e72d1c8ee863d010484
kamildevp <kamil.k52645@gmail.com>
2025-10-26 19:53:28 +0100

    Updated password regex

--- src/Validator/Constraints/Compound/PasswordRequirements.php
+++ src/Validator/Constraints/Compound/PasswordRequirements.php
@@ -17,7 +17,7 @@ class PasswordRequirements extends Compound
             new Assert\Sequentially([
                 new Assert\NotBlank(),
                 new Assert\Regex(
-                    pattern: '/^(?=.*[A-Z])(?=.*\d)[A-Z\d!@#$%?&*]{8,20}$/i',
+                    pattern: '/^(?=.*\p{L})(?=.*\d)[\p{L}\d!@#$%?&*]{8,20}$/i',
                     message: 'Password length must be from 8 to 20 characters, can contain special characters(!#$%?&*) and must have at least one letter and digit'
                 )
             ])

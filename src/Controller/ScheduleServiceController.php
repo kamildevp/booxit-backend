135e7c33c58e7c46139b4ee7f1b8377e906d150d
kamildevp <kamil.k52645@gmail.com>
2025-12-27 23:27:54 +0100

    Updated docs for get schedule service availability endpoint

--- src/Controller/ScheduleServiceController.php
+++ src/Controller/ScheduleServiceController.php
@@ -111,8 +111,7 @@ class ScheduleServiceController extends AbstractController
 
     #[OA\Get(
         summary: 'Get schedule service availability',
-        description: 'Returns schedule service availability for the specified date range (up to one month). If no range is provided, the current week is used by default.
-        </br></br>**Note:** Availability is returned in the Europe/Warsaw timezone.'
+        description: 'Returns schedule service availability for the specified date range (up to one month). If no range is provided, the current week is used by default.'
     )]
     #[AvailabilityResponseDoc]
     #[ValidationErrorResponseDoc]
